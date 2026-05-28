fc8aa598daf07cd6691825fb5cdce23b2bbfa7d8
MekalaNanathakumar <mekalanantha@gmail.com>
2026-05-28 12:51:23 +0530

    updated value

--- Palindrome.ts
+++ Palindrome.ts
@@ -8,7 +8,7 @@ function Palindrome(str:string)
         console.log(`${str} is Palindrome `)
     }
     else {
-                console.log(`${str} is not  Palindrome `)
+                console.log(`${str} is not  Palindrome  updated to sprint branch`)
     }
 
 }
